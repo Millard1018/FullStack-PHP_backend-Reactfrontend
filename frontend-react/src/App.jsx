a13954a2b72f3bb9c9d12a8501bc4e8cd24a2c56
Millard1018 <millardlirio@gmail.com>
2025-09-21 16:15:36 +0800

    Refactor task management logic and UI in App component

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -6,11 +6,13 @@ function App() {
 
   const [tasks, setTasks] = useState([]);
   const [error, setError] = useState(null);
-  const [checkBox, setCheckBox] = useState(false);
-  const [editing, setEditing] = useState(false);
   const [id, setID] = useState('');
   const [newTasks, setNewTasks] = useState('');
-  const [update, setUpdate] = useState('');
+  const [update, setUpdate] = useState({
+    'id': '', 'title': '', 'completed': false
+  });
+  const [refresh, setRefresh] = useState(false);
+  const [checkbox, setCheckbox] = useState(false);
 
   async function getTask() {
       try {
@@ -28,7 +30,7 @@ function App() {
 
   useEffect(() => {
     getTask();
-  }, [])
+  }, [refresh])
 
   
 
@@ -38,7 +40,7 @@ function App() {
         const res = await fetch(`http://127.0.0.1:8000/api/tasks/${id}`, {
           method: 'PUT',
           headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({})
+          body: JSON.stringify({'title': update.title, 'completed': update.completed})
         })
         if (!res.ok) {
           throw new Error({error: 'Unable to update task'})
@@ -48,8 +50,10 @@ function App() {
         setTimeout(() => {setError(err.error)}, 3000)
       }
     }
-    setEditing(false);
     updTask();
+    setUpdate(prev => ({...prev, 'id': '', 'title': '', 'completed': false}));
+    setID('');
+    setRefresh(!refresh);
   }
 
   function deleteTask(id) {
@@ -66,6 +70,7 @@ function App() {
       }
     }
     delTask();
+    setRefresh(!refresh);
   }
 
   const handleSubmit = (e) => {
@@ -81,41 +86,54 @@ function App() {
           throw new Error({error: 'Unable to set new task'});
         }
         await getTask();
+        setNewTasks('');
       } catch(err) {
         setTimeout(() => {setError(err.error)}, 3000);
       }
     }
     setTask();
+    setRefresh(!refresh);
   }
 
   return (
     <>
       <div className="h-screen w-screen flex flex-col justify-center items-center bg-slate-200 space-y-[10vh]" >
         <h1 className="font-bold text-3xl" >Task Manager</h1>
-        <div >
+        <div className="bg-white p-10 rounded-3xl" >
           {tasks?.map(task => (
             <div className="flex justify-center items-center space-x-[3vw] space-y-[3vh]" key={task.id}>
-              <div className="flex space-x-[3vw] mt-[2vh]">
-                {(id !== task.id) && <p className={`font-bold text-xl`} >{task.title}</p>}
-                {(id === task.id) && <input type="text" placeholder={task.title} value={update} onChange={(e) => setUpdate(e.target.value)} />}
-                <button onClick={() => {
-                  setEditing(!editing);
-                  setID((prev) => (prev === task.id ? '' : task.id));
-                }}><Pencil className="active:opacity-100 hover:opacity-70" /></button>
+              <div className="flex space-x-[3vw] mt-[2vh] flex-1">
+
+                {(id !== task.id) && <p className={`font-bold text-xl flex-1`} >{task.title}</p>}
+                {(id === task.id) && <input type="text" placeholder={task.title} value={update.title} onChange={(e) => setUpdate(prev => ({...prev, 'id': task.id, 'title': e.target.value,}))} 
+                className="py-[0.5vh] px-[2vw] flex-1"  />}
+                <button className="flex-4"
+                 onClick={() => {setID((prev) => (prev === task.id ? '' : task.id));}}>
+                  <Pencil className="active:opacity-100 hover:opacity-70" />
+                </button>
+
               </div>
-              <input type="checkbox" checked={checkBox} onChange={(e) => setCheckBox(e.target.checked)} className="w-[3vw] h-[3vh]" />
-              <button onClick={editing ? () => {updateTask(task.id)} : () => {alert("Unable to update, you must edit title first")} } 
-              className="bg-blue-400 p-2 rounded-3xl hover:bg-blue-500 active:bg-blue-600" >Update</button>
-              <button onClick={() => {deleteTask(task.id)}} 
-              className="bg-red-400 p-2 rounded-3xl hover:bg-red-500 active:bg-red-600">Delete</button>
+
+              <div className="flex flex-1 space-x-[3vw]" >
+                <input type="checkbox" checked={update.id === task.id ? checkbox : task.completed} onChange={(e) => {
+                  setCheckbox(checkbox ? false : true);
+                  setUpdate(prev => ({...prev, 'id': task.id, 'completed': e.target.checked}));
+                  }} className="w-[3vw] h-[3vh]" />
+                <button onClick={(update.title || update.completed) ? () => {updateTask(task.id)} : () => {alert("Unable to update, you must edit title or un/tick checkbox first")} } 
+                className="bg-blue-400 p-2 rounded-3xl hover:bg-blue-500 active:bg-blue-600" >Update</button>
+                <button onClick={() => {deleteTask(task.id)}} 
+                className="bg-red-400 p-2 rounded-3xl hover:bg-red-500 active:bg-red-600">Delete</button>
+              </div>
+              
             </div>
           ))}
         </div>
-        <form onSubmit={handleSubmit} className=" flex flex-col space-y-[2vh] " >
+        <form onSubmit={handleSubmit} className=" flex flex-col space-y-[2vh] bg-white rounded-3xl p-10 px-15" >
           <input type="text" placeholder='Add Task Title'
-          className="p-2 px-[2vw] rounded-lg" value={newTasks} onChange={(e) => setNewTasks(e.target.value)} />
+          className="p-2 px-[2vw] rounded-lg bg-slate-200" value={newTasks} onChange={(e) => setNewTasks(e.target.value)} />
           <button type='submit' className='bg-gray-400 mx-auto p-2 rounded-3xl hover:bg-gray-500 active:bg-gray-600 ' >Submit</button>
         </form>
+        {error && <p>{error}</p>}
       </div>
     </>
   )
